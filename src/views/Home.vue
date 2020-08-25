c2627862e52deabaf360a3df8ee9783a30be8396
Wangshixin <7864342+wang_shixin@user.noreply.gitee.com>
2020-08-25 18:35:36 +0800

    王世心

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,60 @@
 <template>
  <div>
 
-<view-link></view-link>
+<div class="box">
+  <div class="box1">
+    <img src="../../public/切图/logo.png" alt="" style="width:100%;">
+  </div>
+  <div>
+    <span style="font-size:20px">关于我们</span>
+  </div>
+  
+  <div style="display:flex">
+    <div>
+ <div style="height:60%">
+      <img src="../../public/切图/case/dc5cce2a235033f69db13ed5e9f2a59.png" alt="" style="width:60%;height:80%">
+    </div>
+    <div>我的</div>
+    </div>
+   <div>
+<div style="height:60%">
+      <img src="../../public/切图/case/82630c2820e2cca8801a238754f9ad8.png" alt="" style="width:60%;height:80%">
+    </div>
+    <div>导航</div>
+   </div>
+    
+  </div>
  
+</div>
+  <div>
+      <img src="../../public/切图/banner3.jpg" alt="" style="width:100%">
+    </div>
+    <p style="font-size:25px;text-align:center">房先森简介</p>
+    <p style="text-align:center"><img src="../../public/切图/common/icon_title.png" alt=""></p>
+    <div style="font-size:20px;color:#333">
+     <span>&nbsp;</span><span>&nbsp;</span>房先森互联网装修平台由杭州瑞壹
+      网络科技有限公司于2015年成立上线，房先森
+      立足刚需房，专注于家庭装修服务，利用互联网将
+      装修、供应链、金融进行融合、多维度实现用户对
+      设计师、工长、工地、材料等的先上选择体验，
+      房先森以自营的模式真正意义上过滤掉传统家装公司
+      利润，将利润让给业主。房先森与银../../public/切图/common/icon_title.png行合作的专项
+      装修贷款能够解决业主装修资金问题，并以装修套餐
+      +个性化定制的模式，提供创新型装修服务。
+    </div>
+    <p style="font-size:22px;text-align:center">我们是谁，我们如何与众不同</p>
+    <p style="text-align:center"><img src="../../public/切图/common/icon_title.png" alt=""></p>
+    <div>
+      <img src="../../public/切图/2.png" alt="" style="width:100%">
+    </div>
+    <p style="font-size:22px;text-align:center">服务热线:400-9977-658</p>
+    <div class="ad">
+      <div style="color:green">触屏版</div>
+      <div>电脑版</div>
+      <div>关于我们</div>
+      <div>联系我们</div>
+    </div>
+    <p style="color:grey;font-size:13px;text-align:center">2016sirfang.com刚需房装修就找房先森</p>
   </div>
 </template>
 
@@ -13,5 +65,22 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+.box{
+  display: flex;
+  justify-content: space-between;
+}
+.box1{
+  width: 40%;
+  height: 100%;
+}
+.ad{
+  display: flex;
+  justify-content: space-around;
+  font-size: 17px;
 
+}
 </style>
